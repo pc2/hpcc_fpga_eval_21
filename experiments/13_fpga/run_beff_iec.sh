0997d3cdbe3f2f170892f7582fe77e0555ac70cb
Marius Meyer <mariusme@mail.upb.de>
2021-10-13 18:45:39 +0200

    Change node selection for 13 Nodes

--- experiments/13_fpga/run_beff_iec.sh
+++ experiments/13_fpga/run_beff_iec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH -p fpga
-#SBATCH -w fpga-0002,fpga-0003,fpga-0004,fpga-0005,fpga-0006,fpga-0007,fpga-0008,fpga-0009,fpga-0010,fpga-0011,fpga-0012,fpga-0013,fpga-0014
+#SBATCH -w fpga-0001,fpga-0002,fpga-0003,fpga-0004,fpga-0005,fpga-0006,fpga-0007,fpga-0008,fpga-0011,fpga-0012,fpga-0013,fpga-0014,fpga-0015
 #SBATCH -N 13
 #SBATCH -n 26
 #SBATCH -J beff
