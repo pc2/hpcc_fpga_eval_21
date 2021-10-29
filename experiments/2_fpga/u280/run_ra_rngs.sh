694d164fc89e6a376a36a49578d8ddedf03966f8
Marius Meyer <marius.meyer@uni-paderborn.de>
2021-10-29 19:14:57 +0200

    Add measurement results for STREAM and RA

---
+++ experiments/2_fpga/u280/run_ra_rngs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#
+##
+#
+# Execute the benchmarks synthesized with the HPC BSP on 1 FPGA.
+# Results are stored in separate files for each benchmark
+#
+
+mpirun -n 4 -H alveo3b,alveo3c,alveo4b,alveo4c ../../../synthesis_artifacts/RandomAccess/u280-2020.2-xdma-201920-3/RandomAccess_xilinx -f ../../../synthesis_artifacts/RandomAccess/u280-2020.2-xdma-201920-3/random_access_kernels_single.xclbin -r 2 -g 3 -d 29 -n 10 --device 0 \
+| tee ra_N2.out
---
+++ experiments/2_fpga/u280/run_stream.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#
+##
+#
+# Execute the benchmarks synthesized with the HPC BSP on 1 FPGA.
+# The used FPGA is specified in the whitelist (-w) above.
+# Results are stored in separate files for each benchmark
+#
+
+mpirun -n 4 -H alveo3b,alveo3c,alveo4b,alveo4c ../../../synthesis_artifacts/STREAM/u280-2020.2-xdma-201920-3/STREAM_FPGA_xilinx -f ../../../synthesis_artifacts/STREAM/u280-2020.2-xdma-201920-3/stream_kernels_single.xclbin -r 2 -s 1073741824 --device 0 \
+| tee stream_N2.out
