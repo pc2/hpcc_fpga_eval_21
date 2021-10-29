cd85d1369eb8f5b9eb82c15e1b794810f9e42454
Marius Meyer <marius.meyer@uni-paderborn.de>
2021-10-29 19:20:24 +0200

    Update PTRANS build scripts for U280

--- scripts/synthesis/PTRANS/build_u280.sh
+++ scripts/synthesis/PTRANS/build_u280.sh
@@ -14,26 +14,35 @@ CONFIG_NAMES=($(ls u280/*.cmake))
 
 SCRIPT_PATH=${PWD}
 
-TMP_DIR=${SCRIPT_PATH}/tmp
+STORE_DIR_ROOT=/mnt/scratch/meyermar/synth/ptrans
+# build in memory
+TMP_DIR=/dev/shm/meyermar_synth/ptrans
 TMP_PROJECT_DIR=${TMP_DIR}/HPCC_FPGA
 SYNTH_DIR=${TMP_DIR}/build
 
 mkdir -p ${TMP_DIR}
 
-git clone --branch ${HPCC_FPGA_VERSION} git@github.com:pc2/HPCC_FPGA.git ${TMP_PROJECT_DIR}
+if [ ! -d ${TMP_PROJECT_DIR} ]; then
+    git clone --branch ${HPCC_FPGA_VERSION} https://github.com/pc2/HPCC_FPGA.git ${TMP_PROJECT_DIR}
+fi
 
 BENCHMARK_DIR=${TMP_PROJECT_DIR}/${BENCHMARK_NAME}
 
 for r in "${CONFIG_NAMES[@]}"; do
     SYNTH_NAME=${r}
     BUILD_DIR=${SYNTH_DIR}/${SYNTH_NAME}
+    STORE_DIR=${STORE_DIR_ROOT}/${SYNTH_NAME}
 
     mkdir -p ${BUILD_DIR}
+    mkdir -p $(dirname ${STORE_DIR})
     cd ${BUILD_DIR}
 
-    cmake ${BENCHMARK_DIR} -DCMAKE_BUILD_TYPE=Release -DHPCC_FPGA_CONFIG=${SCRIPT_PATH}/${r}
+    cmake ${BENCHMARK_DIR} -DCMAKE_BUILD_TYPE=Release -DHPCC_FPGA_CONFIG=${SCRIPT_PATH}/${r} | tee ${STORE_DIR}_configure.log
 
-    make "${BM_MAKE_TARGETS[@]}"
+    make "${BM_MAKE_TARGETS[@]}" | tee ${STORE_DIR}_build.log
+
+    # Save synthesis in scratch
+    cp -r ${BUILD_DIR}/bin ${STORE_DIR}
 
 done
 
