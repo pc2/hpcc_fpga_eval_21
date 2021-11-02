9dbdbd6eedeccd5c195dc85c6d8e84c1505a889b
Marius Meyer <marius.meyer@uni-paderborn.de>
2021-11-02 15:30:17 +0100

    Add first execution script for U280

---
+++ experiments/1_fpga/u280/run_hpl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#
+##
+#
+# Execute the benchmarks synthesized with the HPC BSP on 1 FPGA.
+# The used FPGA is specified in the whitelist (-w) above.
+# Results are stored in separate files for each benchmark
+#
+
+mpirun -n 1 ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -r 2 -m 96 --device 0 \
+| tee hpl_N1.out
