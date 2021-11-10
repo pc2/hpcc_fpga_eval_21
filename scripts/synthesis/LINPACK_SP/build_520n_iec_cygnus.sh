048323c83465f446939724055f499bc76fb375b5
Marius Meyer <marius.meyer@uni-paderborn.de>
2021-11-10 23:33:13 +0900

    Clean up Cygnus HPL scripts

--- scripts/synthesis/LINPACK_SP/build_520n_iec_cygnus.sh
+++ scripts/synthesis/LINPACK_SP/build_520n_iec_cygnus.sh
@@ -51,7 +51,7 @@ if [ ! -d ${TMP_PROJECT_DIR} ]; then
 fi
 BENCHMARK_DIR=${TMP_PROJECT_DIR}/LINPACK
 
-CONFIG_NAMES=("Nallatech_520N_IEC_B9_SB3_R5_s1" "Nallatech_520N_IEC_B9_SB3_R5_s1_ni")
+CONFIG_NAMES=("Nallatech_520N_IEC_B9_SB3_R5_s1_ni")
 
 for r in "${CONFIG_NAMES[@]}"; do
     SYNTH_NAME=${INTEL_SDK}-${r}
