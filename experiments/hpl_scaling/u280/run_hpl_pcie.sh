fb2f8391fb7c1ff7fa5ad535b92e310130835a67
Marius Meyer <marius.meyer@uni-paderborn.de>
2021-11-11 11:57:51 +0100

    Add HPL scaling results for U280

--- experiments/hpl_scaling/u280/run_hpl_pcie.sh
+++ experiments/hpl_scaling/u280/run_hpl_pcie.sh
@@ -2,10 +2,10 @@
 
 for i in {1..30}; do
     blocks=$i
-    mpirun -n 1 ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -n 1 -m ${blocks} -r 2 -b 8 --device 0 | tee m${blocks}_pcie.txt
+    mpirun -n 1 ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -n 5 -m ${blocks} -r 2 -b 8 --device 0 | tee m${blocks}_pcie.txt
 done
 
 for i in {1..7}; do
     blocks=$((30+$i*5))
-    mpirun -n 1 ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -n 1 -m ${blocks} -r 2 -b 8 --device 0 | tee m${blocks}_pcie.txt
+    mpirun -n 1 ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -n 5 -m ${blocks} -r 2 -b 8 --device 0 | tee m${blocks}_pcie.txt
 done
