67d71e615ecbe40e94aa8f356ffd2342c8abf22a
Marius Meyer <marius.meyer@uni-paderborn.de>
2021-11-15 16:24:56 +0100

    Add GEMM results for 1 FPGA

--- experiments/1_fpga/u280/run_gemm.sh
+++ experiments/1_fpga/u280/run_gemm.sh
@@ -7,5 +7,5 @@
 # Results are stored in separate files for each benchmark
 #
 
- mpirun -n 1 ../../../synthesis_artifacts/GEMM/u280-2020.2-xdma-201920-3/GEMM_xilinx -f ../../../synthesis_artifacts/GEMM/u280-2020.2-xdma-201920-3/gemm_base.xclbin -m 90 --device 0 \
+mpirun -n 1 ../../../synthesis_artifacts/GEMM/u280-2020.2-xdma-201920-3/GEMM_xilinx -f ../../../synthesis_artifacts/GEMM/u280-2020.2-xdma-201920-3/gemm_base.xclbin -m 90 --device 0 --skip-validation \
  | tee gemm_N1.out
