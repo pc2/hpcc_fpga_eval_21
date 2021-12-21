c2dcdb01718ff7d841ac495e503e998b97a36d56
Marius Meyer <mariusme@mail.upb.de>
2021-12-21 17:46:30 +0100

    Add measurements on 520n

--- experiments/1_fpga/run_hpl_pcie.sh
+++ experiments/1_fpga/run_hpl_pcie.sh
@@ -4,11 +4,12 @@
 #SBATCH -N 1
 #SBATCH -n 1 
 #SBATCH -t 2:00:00 
-#SBATCH -w fpga-0002
 #SBATCH -o hpl_pcie_N1_m48-%j.txt
 #SBATCH -e hpl_pcie_N1_m48-%j.txt
 #SBATCH -J HPL
 
+export OMP_NUM_THREADS=1
+
 module load intel intelFPGA_pro/21.2.0 bittware_520n/20.4.0_hpc devel/CMake/3.15.3-GCCcore-8.3.0
 
-srun ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-pcie/Linpack_intel -f ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-pcie/hpl_torus_PCIE.aocx -n 10 -m 48
+srun ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-pcie/Linpack_intel -f ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-pcie/hpl_torus_PCIE.aocx --platform 2 -n 10 -m 48
--- experiments/8_fpga/run_hpl_pcie.sh
+++ experiments/8_fpga/run_hpl_pcie.sh
@@ -4,11 +4,12 @@
 #SBATCH -N 8
 #SBATCH -n 16 
 #SBATCH -t 2:00:00 
-#SBATCH -w fpga-0001,fpga-0002,fpga-0004,fpga-0005,fpga-0006,fpga-0007,fpga-0008,fpga-0009
 #SBATCH -o hpl_pcie_N8_m48-%j.txt
 #SBATCH -e hpl_pcie_N8_m48-%j.txt
 #SBATCH -J HPL
 
+export OMP_NUM_THREADS=1
+
 module load intel intelFPGA_pro/21.2.0 bittware_520n/20.4.0_hpc devel/CMake/3.15.3-GCCcore-8.3.0
 
-srun ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-pcie/Linpack_intel -f ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-pcie/hpl_torus_PCIE.aocx -n 10 -m 48
+srun ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-pcie/Linpack_intel -f ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-pcie/hpl_torus_PCIE.aocx --platform 2 -n 10 -m 48
