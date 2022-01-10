e3efea539cd905ef2124cfeb72083f23a792a1a4
Marius Meyer <marius.meyer@uni-paderborn.de>
2022-01-10 16:03:03 +0100

    Initial results U280 HPL scaling

---
+++ experiments/hpl_scaling/u280/run_hpl_pcie_full.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export OMP_NUM_THREADS=16
+
+for i in {1..30}; do
+    blocks=$(($i*2))
+    mpirun -n 1 ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -n 5 -m ${blocks} -r 2 -b 8 --device 0 | tee m${blocks}_pcie.txt
+done
+
+for i in {1..7}; do
+    blocks=$((60+$i*10))
+    mpirun -n 1 ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -n 5 -m ${blocks} -r 2 -b 8 --device 0 | tee m${blocks}_pcie.txt
+done
