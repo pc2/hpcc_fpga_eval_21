ac434e11c82f76adc48b8e9769037cd42676bea9
Marius Meyer <mariusme@mail.upb.de>
2022-01-10 15:46:08 +0100

    Add missing HPL results 520n

--- experiments/13_fpga/run_hpl_iec.sh
+++ experiments/13_fpga/run_hpl_iec.sh
@@ -3,13 +3,18 @@
 #SBATCH --constraint=20.4.0_max 
 #SBATCH -N 13
 #SBATCH -n 25 
+#SBATCH -w fpga-[0004-0016]
 #SBATCH -t 2:00:00 
-#SBATCH -w fpga-0001,fpga-0002,fpga-0003,fpga-0004,fpga-0005,fpga-0006,fpga-0007,fpga-0008,fpga-0009,fpga-0012,fpga-0013,fpga-0014,fpga-0015
 #SBATCH -o hpl_N13_m48-%j.txt
 #SBATCH -e hpl_N13_m48-%j.txt
 #SBATCH --fpgalink="torus5"
 #SBATCH -J HPL
 
 module load intel intelFPGA_pro/21.2.0 bittware_520n/20.4.0_max devel/CMake/3.15.3-GCCcore-8.3.0
+module load changeFPGAlinks
 
-srun ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-iec/Linpack_intel -f ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-iec/hpl_torus_IEC.aocx -n 10 -m 48
+srun -n 1 changeFPGAlinks --fpgalink="torus5"
+
+sleep 60
+
+srun ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-iec/Linpack_intel -f ../../synthesis_artifacts/LINPACK_SP/520n-21.2.0-20.4.0-iec/hpl_torus_IEC.aocx --platform 1 -n 2 -m 48
