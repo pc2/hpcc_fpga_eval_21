f46bfdf697683bbc8eabb545b06215633e3f0670
Marius Meyer <marius.meyer@uni-paderborn.de>
2022-01-20 12:33:50 +0100

    Update 1 FPGA measurement U280

--- experiments/1_fpga/u280/run_hpl.sh
+++ experiments/1_fpga/u280/run_hpl.sh
@@ -7,7 +7,7 @@
 # The used FPGA is specified in the whitelist (-w) above.
 # Results are stored in separate files for each benchmark
 #
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=15
 
 mpirun -n 1 ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -r 2 -m 96 -n 1 --device 0 \
 | tee hpl_N1.out
