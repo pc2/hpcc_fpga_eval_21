a5c5dc2bbd66d114a69fa678b86ae714343f2754
Marius Meyer <marius.meyer@uni-paderborn.de>
2022-01-21 11:46:31 +0100

    Update measurements U280

--- experiments/2_fpga/u280/run_hpl_strong.sh
+++ experiments/2_fpga/u280/run_hpl_strong.sh
@@ -7,6 +7,6 @@
 # The used FPGA is specified in the whitelist (-w) above.
 # Results are stored in separate files for each benchmark
 #
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=15
 
 mpirun -n 4 -H alveo3b,alveo3c,alveo4b,alveo4c ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/Linpack_xilinx -f ../../../synthesis_artifacts/LINPACK_SP/u280-2020.2-xdma-201920-3/hpl_torus_PCIE.xclbin -r 2 -m 48 -n 5 --device 0 | tee hpl_strong_N2.out
