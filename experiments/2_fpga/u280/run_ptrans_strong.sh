1ea01027e74adc0dd7487c8611ef51e3e9e9b2c1
Marius Meyer <marius.meyer@uni-paderborn.de>
2022-02-10 13:44:06 +0100

    Update PTRANS results U280

--- experiments/2_fpga/u280/run_ptrans_strong.sh
+++ experiments/2_fpga/u280/run_ptrans_strong.sh
@@ -7,4 +7,4 @@
 # Results are stored in separate files for each benchmark
 #
 
-mpirun -n 4 -H alveo3b,alveo3c,alveo4b,alveo4c ../../../synthesis_artifacts/PTRANS/u280-2020.2-xdma-201920-3/Transpose_xilinx -f ../../../synthesis_artifacts/PTRANS/u280-2020.2-xdma-201920-3/transpose_PQ_PCIE.xclbin -n 10 -m 64 --device 0 | tee ptrans_strong_N2.out
+mpirun -n 4 -H alveo3b,alveo3c,alveo4b,alveo4c ../../../synthesis_artifacts/PTRANS/u280-2020.2-xdma-201920-3/Transpose_xilinx -f ../../../synthesis_artifacts/PTRANS/u280-2020.2-xdma-201920-3/transpose_PQ_PCIE.xclbin -n 10 -m 120 --device 0 | tee ptrans_strong_N2.out
